185a3d579d5c8c71a286a0bfc0df8afc44a29e87
GD7 <GD7@DESKTOP-FCHUFHV>
2020-01-02 16:26:12 +0900

    경민 스프링 변환

---
+++ src/main/java/kr/gudi/admin/adminService.java
@@ -0,0 +1,8 @@
+package kr.gudi.admin;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class adminService {
+
+}
---
+++ src/main/webapp/resources/js/admin.js
@@ -0,0 +1,49 @@
+/**
+ * adminPage JS
+ */
+
+/*Modal 변수선언*/
+var openButton;
+var modal;
+var overlay;
+var closeBtn;
+/*Tab Menu*/
+function menu(id) {
+	var oldid = document.querySelector('.show');
+	oldid.classList.remove('show');
+	var id = document.querySelector(id);
+	id.classList.add('show');
+}
+/*LOGO바로가기*/
+function home(url) {
+	window.open(url, "_blank");
+}
+/*Modal*/
+function openModal(MDnum) {
+	 modal = document.querySelector(MDnum);
+	 modal.classList.remove("hidden");
+	 overlay = document.getElementsByClassName("modal__overlay");
+	 openButton = document.getElementById("open");
+}
+function closeModal() {
+	 closeBtn = document.getElementsByClassName("close");
+	 modal.classList.add("hidden");
+}
+
+/*accordion*/
+function Accordion(){
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	    this.classList.toggle("active");
+	    var panel = this.nextElementSibling;
+	    if (panel.style.display === "block") {
+	      panel.style.display = "none";
+	    } else {
+	      panel.style.display = "block";
+	    }
+	  });
+	}
+}
