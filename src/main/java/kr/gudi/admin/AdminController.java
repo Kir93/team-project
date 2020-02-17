3795c7fda1e7413c1964e6d4963b89f77ca9b2c1
GD7 <GD7@DESKTOP-B9SRSNV>
2020-02-17 11:09:14 +0900

    경민 test1

--- src/main/java/kr/gudi/admin/AdminController.java
+++ src/main/java/kr/gudi/admin/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller 
-public class AdminController {
+public class AdminController { 
 
 	@Autowired AdminService adminService;
 	
